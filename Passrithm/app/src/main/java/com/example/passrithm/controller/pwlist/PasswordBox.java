43d173a81ae10a5aa212fbb84c931a87b6966b6b
권민선 <20alstjs02@naver.com>
2022-11-21 21:19:17 +0900

    feat:password list연동 && shareButton활성화

--- Passrithm/app/src/main/java/com/example/passrithm/controller/pwlist/PasswordBox.java
+++ Passrithm/app/src/main/java/com/example/passrithm/controller/pwlist/PasswordBox.java
@@ -1,14 +1,10 @@
 package com.example.passrithm.controller.pwlist;
 
 public class PasswordBox {
-    String domain;
     String password;
-    int id;
-    Boolean isPopUp;
-    public PasswordBox(String domain, String password,int id,boolean isPopUp){
-        this.domain=domain;
+
+    public PasswordBox(String password){
         this.password=password;
-        this.isPopUp=isPopUp;
-        this.id=id;
+
     }
 }
