531fac501a379a3c85674f35f4115518483bb972
0zlrlo <99941493+0zlrlo@users.noreply.github.com>
2022-11-29 16:29:38 +0900

    feat : push main

--- Passrithm/app/src/main/java/com/example/passrithm/controller/base/SignupActivity.java
+++ Passrithm/app/src/main/java/com/example/passrithm/controller/base/SignupActivity.java
@@ -59,7 +59,7 @@ public class SignupActivity extends AppCompatActivity {
         signup.setOnClickListener(onClickListener);
     }
 
-        View.OnClickListener onClickListener= new View.OnClickListener() {
+        View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v){
                 switch (v.getId()){
