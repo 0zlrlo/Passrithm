4cc55403a8e2b882db26518e95a8a8be3952cf0d
minheeLee <87891501+MoOo2mini@users.noreply.github.com>
2022-12-10 18:07:13 +0900

    feat : export image

--- Passrithm/app/src/main/java/com/example/passrithm/controller/pwlist/ExportImgFragment.java
+++ Passrithm/app/src/main/java/com/example/passrithm/controller/pwlist/ExportImgFragment.java
@@ -1,2 +1,44 @@
-package com.example.passrithm.controller.pwlist;public class ExportImgFragment {
+package com.example.passrithm.controller.pwlist;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.passrithm.R;
+
+import java.util.List;
+
+public class ExportImgFragment extends Fragment {
+    private View view;
+    private List<PasswordBox> passwordBoxes;
+
+    public ExportImgFragment(List<PasswordBox> passwordBoxes) {
+        this.passwordBoxes = passwordBoxes;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_export_img, container, false);
+
+        TextView passwordTv = view.findViewById(R.id.password_tv);
+        passwordTv.setText(setPassword(passwordBoxes));
+
+        return view;
+    }
+
+    private String setPassword(List<PasswordBox> passwordBoxes) {
+        String result = "";
+        for (PasswordBox passwordBox : passwordBoxes) {
+            result += passwordBox.toString();
+        }
+
+        return result;
+    }
 }
